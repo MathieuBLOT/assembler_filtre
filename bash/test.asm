8e1626d7afd9ca0ab5e6e7a7e9cd2769382ccfee
Thibaud Backenstrass <thibaud.backenstrass@phelma.grenoble-inp.fr>
2015-03-20 18:42:03 +0100

    Ajout d'un fichier de test

---
+++ bash/test.asm
@@ -0,0 +1,34 @@
+; Fichier assembleur de test, pour l'implémentation BASH
+add r1, r2, r3
+SUB r1 r2 r3
+INC r1 r2
+dec R1 R2
+addc r1 r2 r3
+subc $1 $2 $3
+mova r1 r2
+movb r1 r2
+nega r1 r2
+negb r1 r2
+and r1 r2 r3
+or r1 r2 r3
+xor r1 r2 r3
+not  r1 r2
+shl1 r1 r2
+shl2 r1 r2
+shl3 r1 r2
+shl4 r1 r2
+shl5 r1 r2
+shl6 r1 r2
+shl7 r1 r2
+shl8 r1 r2
+shl9 r1 r2
+shl10 r1 r2
+shl11 r1 r2
+shl12 r1 r2
+shl13 r1 r2
+shl14 r1 r2
+shl15 r1 r2
+shr1 r1 r2
+shr2 r1 r2
+shr3 r1 r2
+shr4 r1 r2
